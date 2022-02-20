553f3f4b9840014369552d3dd20f8c2e9d5fa389
emirgoren <gorenemir@outlook.com>
2022-02-20 10:44:07 +0300

    breaker: done && breaker function and clearing the interval

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-setInterval(() => {
+let netflixStoppedOn = '';
+
+let interval = setInterval(() => {
+    breaker(this);
+}, 1000);
+
+function breaker(interval){
     let subtitleFirstParent = document.getElementsByClassName("player-timedtext")[0];
     let subtitleSpans = subtitleFirstParent.children[0]?.children;
     if(subtitleSpans !== undefined) {
         let subtitle = '';
         for(let i = 0; i < subtitleSpans.length; i++) {
             subtitle += subtitleSpans[i].innerText;
-            if(subtitle.length >= 45){
-                console.log(subtitle);
+            if(subtitle.length >= 45 && subtitle !== netflixStoppedOn){
+                const video = document.getElementsByTagName("video")[0];
+                video.pause();
+                netflixStoppedOn = subtitle;
+
+                clearInterval(interval);
+                setTimeout(() => {
+                    video.play();
+                    interval = setInterval(() => {
+                        breaker(interval);
+                    }, 1000);
+                }, 2500);
             }
         }
     }
-}, 1000);
+}
