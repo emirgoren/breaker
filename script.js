12e458beeba87bb604a6f11fbddb098fd3539e25
emirgoren <gorenemir@outlook.com>
2022-02-20 10:24:46 +0300

    length: log the subtitle if the length >= 45

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
 setInterval(() => {
     let subtitleFirstParent = document.getElementsByClassName("player-timedtext")[0];
     let subtitleSpans = subtitleFirstParent.children[0]?.children;
     if(subtitleSpans !== undefined) {
+        let subtitle = '';
         for(let i = 0; i < subtitleSpans.length; i++) {
-            console.log(subtitleSpans[i].innerText);
+            subtitle += subtitleSpans[i].innerText;
+            if(subtitle.length >= 45){
+                console.log(subtitle);
+            }
         }
     }
 }, 1000);
-
-
-
-// let playerTimedTextContainer = document.getElementsByClassName("player-timedtext-container");
