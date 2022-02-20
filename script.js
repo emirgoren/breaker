73540c1ecf417e9c70acc87e78adf992e24dcb70
emirgoren <gorenemir@outlook.com>
2022-02-20 10:20:08 +0300

    subtitles: reaching to the subtitle spans

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('first-commit');
+
+setInterval(() => {
+    let subtitleFirstParent = document.getElementsByClassName("player-timedtext")[0];
+    let subtitleSpans = subtitleFirstParent.children[0]?.children;
+    if(subtitleSpans !== undefined) {
+        for(let i = 0; i < subtitleSpans.length; i++) {
+            console.log(subtitleSpans[i].innerText);
+        }
+    }
+}, 1000);
+
+
+
+// let playerTimedTextContainer = document.getElementsByClassName("player-timedtext-container");
